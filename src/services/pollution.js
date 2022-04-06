c288027d89428c35123df07138e0f0ba81d60910
jakwrobel <90487772+jakwrobel@users.noreply.github.com>
2022-04-06 16:02:30 +0200

    Add new API key

--- src/services/pollution.js
+++ src/services/pollution.js
@@ -6,6 +6,7 @@ export const keys = [
   "8cb705c6cfmsh0a3c87eb6ca9cf5p14a1a7jsn92aa1ee82eab",
   "dd31c8c14cmsh2ab1c44907f3c88p1bcb1cjsnd1b772d2c469",
   "edabb595a8mshc8280ddb349e608p156bb9jsn8072ab5b0200",
+  "dd31c8c14cmsh2ab1c44907f3c88p1bcb1cjsnd1b772d2c469"
 ];
 
 
